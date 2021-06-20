799ad632b4ec0fe85b67fa89ecfea97a2ea59a2f
Yunus Emre Acar <yunus3emre4@outlook.com>
2021-06-20 21:44:27 +0300

    First Commit

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import Link from 'next/link'
+import { getStortedPostsData } from '../lib/posts'
+import Date from '../components/date'
+
+
+export async function getStaticProps(){
+  const allPostsData = getStortedPostsData()
+  return {
+    props :{
+      allPostsData
+    }
+  }
+}
+
+export default function Home({allPostsData}) {
+  return (
+    <Layout home>
+      <head>
+        <title>{siteTitle}</title>
+      </head>
+      <section className={utilStyles.headingMd}>
+        <p>Hi, I'm Yunus Emre. Welcome To My WebSite</p>
+        <p>
+          (This is a sample website - you'll be building site like this on{' '}
+          <a href="https://nextjs.org/learn"> our Next.js tutorial</a>.  )
+        </p>
+        <h2>
+          Go To <Link href="posts/first-post">
+            <a>Blog</a>
+          </Link>
+        </h2>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({id, date,title})=>(
+          <li className={utilStyles.listItem} key={id}>
+            <Link href={`/posts/${id}`}>
+              <a>{title}</a>
+            </Link>
+            <br />
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
+          </li>
+        ))}
+      </ul>
+      </section>
+    </Layout>
+  )
+}
